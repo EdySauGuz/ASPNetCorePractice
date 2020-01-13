0b8f08554f7f429d47ee9d8c489e6087582a1fd9
Eduardo Sauceda <drukker135@gmail.com>
2020-01-13 11:18:02 -0600

    modificacion datepicker

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AspNetCoreTodo.Models;
+using AspNetCoreTodo.Services;
+
+namespace AspNetCoreTodo.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // Obtener las tareas desde la base de datos
+            var items = await _todoItemService.GetIncompleteItemsAsync();
+            // Colocar los tareas en un modelo
+            var model = new TodoViewModel()
+            {
+                Items = items
+            };
+            // Genera la vista usando el modelo
+            return View(model);
+        }
+
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddItem(TodoItem newItem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            var successful = await _todoItemService.AddItemAsync(newItem);
+            if (!successful)
+            {
+                return BadRequest("No fue posible agregar la tarea.");
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+///  READY
+$$ = jQuery;
+
+$$.when($$.ready).then(function () {
+    $$('.date').data('provide', 'datepicker').datepicker({
+        format: 'dd/mm/yyyy',
+        startDate: '-1d',
+        language: 'es',
+        todayHighlight: true,
+        toggleActive: true
+    });
+    $$('.date').data('provide', 'datepicker').datepicker('update', new Date());
+    $$('.date').data('provide', 'datepicker').datepicker()
+        .on('changeDate', function (e) {
+            $$('.date').data('provide', 'datepicker').datepicker('hide');
+        });
+});
